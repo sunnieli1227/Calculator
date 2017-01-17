92af7634f52c24f45c1385ab3f8e209d956d2b48
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-01-17 11:13:00 -0800

    changed border

--- calculator.js
+++ calculator.js
@@ -23,6 +23,7 @@ function e( ) {
 function colorChange(color) {
 	// find all the buttons
  	document.getElementByClassName("button").className="blue";
+	document.getElementByClassName("button").className="red";
 	// elementsToChange = ["<input>", "<input>"]
 	// cycle through all the elements in the array, changing their class (.classname = "");
 	//var array = [1,"potato",3,4];
